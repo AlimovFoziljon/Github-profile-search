038a6ec0355ab22429ffcfc84630ae422aa77e67
Alimov Foziljon <100414303+AlimovFoziljon@users.noreply.github.com>
2023-03-03 22:38:18 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,61 @@
+const API_URL = 'https://api.github.com/users/'
+
+const input = document.getElementById('input')
+const results = document.getElementById('results')
+const button = document.getElementById('button')
+
+const getUser = async (user) =>{
+    const res = await fetch(API_URL + user)
+    const data = await res.json()
+    getResult(data)
+}
+button.addEventListener('click', (e) =>{
+    e.preventDefault()
+    const inputValue = input.value
+    if(inputValue){
+        getUser(inputValue)
+        input.value = ''
+    }
+})
+
+function getResult(userData) {
+    const resultCard = `
+    <div class="card">
+        <img class="avatar" src=${userData.avatar_url}/>
+        <div class="user-info">
+            <h2>Name: ${userData.name}</h2>
+            <h3>Login: ${userData.login}</h3>
+            <h4>Id: ${userData.id}</h4>
+            <div class="user-statistic">
+                <p>Followers: ${userData.followers}</p>
+                <p>Followers: ${userData.following}</p>
+                <p>Repositories: ${userData.public_repos}</p>
+            </div>
+            <div class"user-links">
+                <div class="link">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#8B949E" class="bi bi-building" viewBox="0 0 16 16">
+                        <path d="M4 2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1Zm3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1Zm3.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1ZM4 5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1ZM7.5 5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1Zm2.5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1ZM4.5 8a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1Zm2.5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1Zm3.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1Z"/>
+                        <path d="M2 1a1 1 0 0 1 1-1h10a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V1Zm11 0H3v14h3v-2.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5V15h3V1Z"/>
+                    </svg>
+                    <p>${userData.company}</p>
+                </div>
+                <div class="link">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#8B949E" class="bi bi-geo-alt" viewBox="0 0 16 16">
+                        <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z"/>
+                        <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                    </svg>
+                    <p>${userData.location}</p>
+                </div>
+                <div class="link">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#8B949E" class="bi bi-twitter" viewBox="0 0 16 16">
+                        <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"/>
+                    </svg>
+                    <p>${userData.twitter_username}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    `
+    results.innerHTML = resultCard
+}
+getUser('AlimovFoziljon')
